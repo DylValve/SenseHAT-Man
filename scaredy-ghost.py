3c1d172fb203ce403972950ec496b4dba4d2b24b
Nathan Chaimongkhon <J222144@tafe.wa.edu.au>
2020-08-24 16:53:36 +0800

    Create Scaredy ghost

---
+++ scaredy-ghost.py
@@ -0,0 +1,52 @@
+"""
+    Ghost: Blinky
+
+    This file defines the frames for Blinky
+
+"""
+
+from sensible_colours import *
+
+blinky_frames = [
+    [
+        k, k, bd, bd, bd, b, k, k,
+        k, bd, bd, bd, bd, bd, b, k,
+        bd, bd, w, k, bd, w, k, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, k, bd, bd, k, bd, b, b
+    ],
+    [
+        k, k, bd, bd, bd, b, k, k,
+        k, bd, bd, bd, bd, bd, b, k,
+        bd, bd, w, k, bd, w, k, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, k, bd, bd, bd, k, b, b
+    ],
+    [
+        k, k, bd, bd, bd, b, k, k,
+        k, bd, bd, bd, bd, bd, b, k,
+        bd, bd, k, w, bd, k, w, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, k, bd, bd, bd, k, b
+    ],
+    [
+        k, k, bd, bd, bd, b, k, k,
+        k, bd, bd, bd, bd, bd, b, k,
+        bd, bd, k, w, bd, k, w, b,
+        bd, bd, w, w, bd, w, w, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, bd, bd, bd, bd, b, b,
+        bd, bd, k, bd, bd, k, b, b
+    ],
+
+]
