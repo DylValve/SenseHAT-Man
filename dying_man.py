83827158596aabe0fbff261bc847f87c0cb158fb
Nathan Chaimongkhon <J222144@tafe.wa.edu.au>
2020-08-24 17:24:35 +0800

    create dying man

---
+++ dying_man.py
@@ -0,0 +1,81 @@
+"""
+    Ghost: Blinky
+
+    This file defines the frames for Blinky
+
+"""
+
+from sensible_colours import *
+
+blinky_frames = [
+    [
+        k, k, y, y, y, yd, k, k,
+        k, y, y, y, y, y, yd, k,
+        y, y, y, y, y, y, yd, yd,
+        y, y, y, y, y, y, yd, yd,
+        y, y, y, y, y, y, yd, yd,
+        y, y, y, y, y, y, yd, yd,
+        k, y, y, y, y, y, yd, k,
+        k, k, y, y, y, yd, k, k
+    ],
+    [
+        k, k, y, y, y, yd, k, k,
+        k, y, y, y, y, y, yd, k,
+        y, y, y, y, y, y, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, y, y, y, k, k,
+        k, y, y, y, y, y, yd, k,
+        k, k, y, y, y, yd, k, k
+    ],
+   [
+        k, k, y, y, y, k, k, k,
+        k, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, k, k, k, k, k,
+        y, y, y, k, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        k, y, y, y, k, k, k, k,
+        k, k, y, y, y, k, k, k
+    ],
+   [
+        k, k, y, y, y, k, k, k,
+        k, y, y, y, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        y, y, y, k, k, k, k, k,
+        y, y, y, k, k, k, k, k,
+        y, y, y, y, k, k, k, k,
+        k, y, y, y, k, k, k, k,
+        k, k, y, y, y, k, k, k
+    ],
+    [
+        k, k, y, k, k, k, k, k,
+        k, y, y, k, k, k, k, k,
+        y, y, k, k, k, k, k, k,
+        y, y, k, k, k, k, k, k,
+        y, y, k, k, k, k, k, k,
+        y, y, k, k, k, k, k, k,
+        k, y, y, k, k, k, k, k,
+        k, k, y, y, k, k, k, k
+    ],
+	[
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        y, k, k, k, k, k, k, k,
+        y, y, k, k, k, k, k, k,
+        y, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k,
+        k, k, k, k, k, k, k, k
+    ],
+	[
+        k, k, k, k, k, k, k, y,
+        k, k, k, k, k, k, y, k,
+        k, k, k, k, k, y, k, k,
+        k, k, k, k, y, k, k, k,
+        k, k, k, k, y, k, y, y,
+        k, k, k, k, k, y, k, k,
+        k, k, k, k, k, k, y, k,
+        k, k, k, k, k, k, k, y
+    ],
+]
